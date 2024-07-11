5b1bb475668d9b9a75426e025bb6e875628baeb4
Nivedita Angadi <nivedita@Niveditas-MacBook-Pro.local>
2024-07-11 20:41:42 +0530

    Initial Project SetUp

---
+++ GetFit/GetFit/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  GetFit
+//
+//  Created by Nivedita Angadi on 11/07/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ GetFit/GetFit/GetFitApp.swift
@@ -0,0 +1,17 @@
+//
+//  GetFitApp.swift
+//  GetFit
+//
+//  Created by Nivedita Angadi on 11/07/24.
+//
+
+import SwiftUI
+
+@main
+struct GetFitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
