05c939e7a0674b25bbceca1a767fa612c36e1545
Nivedita Angadi <nivedita@Niveditas-MacBook-Pro.local>
2024-07-15 11:07:23 +0530

    Updated Git fit app

--- GetFit/GetFit/ContentView.swift
+++ GetFit/GetFit/ContentView.swift
@@ -8,14 +8,48 @@
 import SwiftUI
 
 struct ContentView: View {
+    var activities = ["Archery", "Baseball", "Basketball", "Bowling", "Boxing", "Cricket", "Curling", "Fencing", "Golf", "Hiking", "Lacrosse", "Rugby", "Squash"]
+    
+    var colors: [Color] = [.blue, .cyan, .gray, .green, .indigo, .mint, .orange, .pink, .purple, .red]
+
+    @State private var selected = "Baseball"
+    @State private var id = 1
+
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+    //====
+            Text("Why not try...")
+                .font(.largeTitle.bold())
+    //====
+            Spacer()
+   //=====
+            VStack {
+                Circle()
+                    .fill(colors.randomElement() ?? .blue)
+                    .padding()
+                    .overlay(
+                        Image(systemName: "figure.\(selected.lowercased())")
+                            .font(.system(size: 144))
+                            .foregroundColor(.white)
+                    )
+                Text("\(selected)")
+                    .font(.title)
+            }
+            .transition(.slide)
+            .id(id)
+    //====
+            Spacer()
+    //====
+            Button("Try again") {
+                withAnimation(.easeInOut(duration: 1)) {
+                    selected = activities.randomElement() ?? "Archery"
+                    id += 1
+                }
+            }
+            .buttonStyle(.borderedProminent)
+            
+  // ===== 
         }
-        .padding()
     }
 }
 
